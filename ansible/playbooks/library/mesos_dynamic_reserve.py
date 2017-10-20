1ed7a7ff8c6929b9a706bd6262ca3c423ba42893
sorididim11 <sorididim11@gmail.com>
2017-10-21 00:29:44 +0900

    impove dynamic reservation

--- ansible/playbooks/library/mesos_dynamic_reserve.py
+++ ansible/playbooks/library/mesos_dynamic_reserve.py
@@ -157,7 +157,7 @@ def send_request(token, mesos_url, req):
         "Authorization": "token={}".format(token),
         "Accept": "application/json"
     }
-    print(json.dumps(req))
+    body = "\{ %s \}" % json.dumps(req)
     url = "{}{}".format(mesos_url, '/mesos/api/v1')
     result = requests.post(url, json.dumps(req), headers=headers, verify=False) 
 
