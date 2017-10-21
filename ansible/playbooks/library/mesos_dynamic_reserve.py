44514408d9a410ecc0721cd730778ff6ccaf9fc6
sorididim11 <sorididim11@gmail.com>
2017-10-21 14:58:03 +0900

    impove dynamic reservation

--- ansible/playbooks/library/mesos_dynamic_reserve.py
+++ ansible/playbooks/library/mesos_dynamic_reserve.py
@@ -162,6 +162,8 @@ def send_request(token, mesos_url, req):
 
     url = "{}{}".format(mesos_url, '/mesos/api/v1')
     result = requests.post(url, json.dumps(req), headers=headers, verify=False) 
+
+    print('status code: %d' result.status_code)
     return result
 
 
