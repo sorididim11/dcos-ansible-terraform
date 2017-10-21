067c9e64eef426cf5fde1d90897e0141dc153131
sorididim11 <sorididim11@gmail.com>
2017-10-21 14:19:52 +0900

    impove dynamic reservation

--- ansible/playbooks/library/mesos_dynamic_reserve.py
+++ ansible/playbooks/library/mesos_dynamic_reserve.py
@@ -161,8 +161,7 @@ def send_request(token, mesos_url, req):
         json.dump(req, fp)
 
     url = "{}{}".format(mesos_url, '/mesos/api/v1')
-    # result = requests.post(url, json.dumps(req), headers=headers, verify=False) 
-    result={}
+    result = requests.post(url, json.dumps(req), headers=headers, verify=False) 
     return result
 
 
@@ -180,12 +179,12 @@ def handle_dynamic_reservation(req):
 
     if reserve_req:
         result1 = send_request(token, mesos_url, reserve_req)
-        # if result1.status_code != 202:
-        #     raise Exception(result1.json())
-    # if unreserve_req:
-    #     result2 = send_request(token, mesos_url, unreserve_req)
-    #     if result2.status_code != 202:
-    #         raise Exception(result2.json())
+        if result1.status_code != 202:
+            raise Exception(result1.json())
+    if unreserve_req:
+        result2 = send_request(token, mesos_url, unreserve_req)
+        if result2.status_code != 202:
+            raise Exception(result2.json())
 
     # default: something went wrong
     meta = {"status": 202}
